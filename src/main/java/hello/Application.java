7d67aa47658bc9144292b37ab68931bc659e7ce6
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-08-20 17:56:00 +0300

    springBoot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,16 @@
 package hello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@EnableJpaRepositories("org.baeldung.persistence.repo")
-@EntityScan("org.baeldung.persistence.model")
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/hello/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-class BookNotFoundException extends RuntimeException {
-
-    BookNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    // ...
-}
