50aa492e5177bf49ec6345defe15eecf6ff1dc73
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-08-21 18:05:40 +0300

    springBoot

---
+++ src/main/java/hello/PersonForm.java
@@ -0,0 +1,32 @@
+package hello;
+
+public class PersonForm {
+    private String firstName;
+    private String lastName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
