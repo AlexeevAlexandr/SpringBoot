94d953737fcfbe34fb38179363a7dc4dfe8cfa7a
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-08-31 15:28:41 +0300

    date page

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -7,13 +7,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 
 @Controller
 public class MainController {
@@ -118,5 +116,11 @@ public class MainController {
     @RequestMapping(value = {"/page2"})
     public String page2(){
         return "/page2";
+    }
+
+    @GetMapping("/date")
+    String date (Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "date";
     }
 }
